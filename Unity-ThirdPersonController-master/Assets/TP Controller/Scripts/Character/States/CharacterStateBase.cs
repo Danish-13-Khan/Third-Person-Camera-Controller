acc0f6bd67cadeec99db45cb8a61b041daccf277
danish Khna <danish@appsoleutcoders.com>
2018-09-19 17:35:46 +0530

    Third Person Mobile Controller

---
+++ Unity-ThirdPersonController-master/Assets/TP Controller/Scripts/Character/States/CharacterStateBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class CharacterStateBase : ICharacterState
+{
+    public static readonly ICharacterState GROUNDED_STATE = new GroundedCharacterState();
+    public static readonly ICharacterState JUMPING_STATE = new JumpingCharacterState();
+    public static readonly ICharacterState IN_AIR_STATE = new InAirCharacterState();
+
+    public virtual void OnEnter(Character character) { }
+
+    public virtual void OnExit(Character character) { }
+
+	public virtual void Update(Character character, SimpleTouchController touchControoller, FixedTouchField field)
+    {
+        character.ApplyGravity();
+		character.MoveVector = PlayerInput.GetMovementInput(character.Camera,touchControoller);
+		character.ControlRotation = PlayerInput.GetMouseRotationInput(field.TouchDist.x* 0.75f, field.TouchDist.y* 0.15f);
+    }
+
+    public virtual void ToState(Character character, ICharacterState state)
+    {
+        character.CurrentState.OnExit(character);
+        character.CurrentState = state;
+        character.CurrentState.OnEnter(character);
+    }
+}
